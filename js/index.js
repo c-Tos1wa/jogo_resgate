a5a9a5b713cf532265a30c4fc941aded125cb201
Cristina <cristoshiwassaki@gmail.com>
2021-09-07 19:29:23 -0300

    funções e suas funções descritas em comentários

--- js/index.js
+++ js/index.js
@@ -7,16 +7,38 @@ function start() {
   $(".fundo").append("<div id='partner' class='anima3'></div>");
   
   var game = {}
+  var key = {
+    upArrow: 38,
+    downArrow: 40,
+    D: 68,
+  }
 
+  game.pressed = [];
+
+  $(document).keydown(function(event){
+    game.pressed[event.which] = true;
+  });
+  $(document).keyup(function(event){
+    game.pressed[event.which] = false;
+  });
+
+  //faz a variável entrar em um looping a cada 50 ms;
   game.timer = setInterval(loop, 50);
+
 
   function loop() {
     move();
+
+    movePlayer();
   }
 
+  //move a imagem de fundo de acordo com o setInterval
   function move(){
     left = parseInt($('.fundo').css('background-position'));
     $('.fundo').css('background-position', left-1);
   }
   
+  function movePlayer(){
+    
+  }
 }
